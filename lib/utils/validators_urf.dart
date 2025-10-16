1e9b631b2f88df9ad655cd37ac942098d9140aa7
yasmin hany <162918255+yoyo3257@users.noreply.github.com>
2025-10-16 06:50:15 +0300

    Fix User form validation and add tests

--- lib/utils/validators_urf.dart
+++ lib/utils/validators_urf.dart
@@ -1,6 +1,6 @@
 bool isValidEmail(String email) {
   return RegExp(
-    r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
+    r"^[a-zA-Z0-9.a-zA-Z0-9!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
   ).hasMatch(email);
 }
 
